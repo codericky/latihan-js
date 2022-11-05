3bfd9a1d30f23519f2851d62a29d7819b5f6487c
codericky <thevenom425@gmail.com>
2022-11-05 15:45:12 +0700

    edit

---
+++ script.js
@@ -0,0 +1,14 @@
+// var aksi = "";
+// aksi = () => {
+//     return "Hello World"
+// }
+// document.getElementById("aic").innerHTML 
+// = "Hasil " + aksi();
+
+aksi = (a, b) => { c = 5;
+    return (a + b) * c;
+ };
+
+document.getElementById("aic").innerHTML 
+= "Hasil Aritmatika " + (aksi(20, 10));
+      
